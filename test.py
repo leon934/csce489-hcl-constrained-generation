8785dc113e47124fe2a5bec7fc96723978238c1e
Lyle Morris <carlylemorris@gmail.com>
2025-11-30 12:11:28 -0600

    POC

---
+++ test.py
@@ -0,0 +1,17 @@
+import json
+from llama_cpp import Llama
+
+llm = Llama(
+    model_path="./models/Phi-3-mini-4k-instruct-q4.gguf",
+)
+
+resp = llm.create_chat_completion(
+	messages = [
+		{
+			"role": "user",
+			"content": "What is the capital of France?"
+		}
+	]
+)
+
+print(json.dumps(resp, indent=2))
