4363487dccbb06d36dedb5879cc95b0f3f6ccac2
Lyle Morris <carlylemorris@gmail.com>
2025-11-30 12:12:08 -0600

    add link

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import json
 from llama_cpp import Llama
 
 llm = Llama(
-    model_path="./models/Phi-3-mini-4k-instruct-q4.gguf",
+    model_path="./models/Phi-3-mini-4k-instruct-q4.gguf", #https://huggingface.co/microsoft/Phi-3-mini-4k-instruct-gguf
 )
 
 resp = llm.create_chat_completion(
