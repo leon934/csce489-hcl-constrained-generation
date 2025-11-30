9f86d23534642f78290e45347b7b580b3e7b66fe
Lyle Morris <carlylemorris@gmail.com>
2025-11-30 12:26:08 -0600

    grammar test

--- test.py
+++ test.py
@@ -1,17 +1,15 @@
 import json
-from llama_cpp import Llama
+from llama_cpp import Llama, LlamaGrammar
+
+grammar = LlamaGrammar.from_file("grammar.gbnf")
 
 llm = Llama(
     model_path="./models/Phi-3-mini-4k-instruct-q4.gguf", #https://huggingface.co/microsoft/Phi-3-mini-4k-instruct-gguf
 )
 
-resp = llm.create_chat_completion(
-	messages = [
-		{
-			"role": "user",
-			"content": "What is the capital of France?"
-		}
-	]
+resp = llm.create_completion(
+	prompt="Is Paris the capitol of France? ",
+    grammar=grammar
 )
 
 print(json.dumps(resp, indent=2))
